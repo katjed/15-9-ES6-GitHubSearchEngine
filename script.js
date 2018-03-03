99697431b59c402a9530d17a106706efe02d31d5
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-03-03 20:29:10 +0100

    Update files

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ class UsersList extends React.Component {
 
   	render() {
     	return (
-      		<div>
+      		<div className="users">
         		{this.users}
       		</div>
     	);
@@ -54,7 +54,7 @@ class UsersList extends React.Component {
 class User extends React.Component {
   	render() {
     	return (
-      		<div>
+      		<div className="user">
         		<img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
         		<a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
       		</div>
